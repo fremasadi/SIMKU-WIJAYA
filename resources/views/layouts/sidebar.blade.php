8dd6e4c7fb6af692090e9f6b66cbb17ad3e6dbb1
fremasadi <fremasadii@gmail.com>
2026-01-14 00:15:53 +0700

    bimbingan 1

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -8,8 +8,7 @@
             <span class="app-brand-text demo menu-text fw-bolder ms-2">simku-wijaya</span>
         </a>
 
-        <a href="javascript:void(0);"
-           class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
+        <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
             <i class="bx bx-chevron-left bx-sm align-middle"></i>
         </a>
     </div>
@@ -18,32 +17,32 @@
 
     <ul class="menu-inner py-1">
 
-    <!-- Dashboard -->
-    @if(auth()->user()->role != 'kasir')
-    <li class="menu-item {{ request()->is('dashboard') ? 'active' : '' }}">
-        <a href="{{ url('/dashboard') }}" class="menu-link">
-            <i class="menu-icon tf-icons bx bx-home-circle"></i>
-            <div>Dashboard</div>
-        </a>
-    </li>
-    @endif
+        <!-- Dashboard -->
+        @if (auth()->user()->role != 'kasir')
+            <li class="menu-item {{ request()->is('dashboard') ? 'active' : '' }}">
+                <a href="{{ url('/dashboard') }}" class="menu-link">
+                    <i class="menu-icon tf-icons bx bx-home-circle"></i>
+                    <div>Dashboard</div>
+                </a>
+            </li>
+        @endif
 
-    <!-- TRANSAKSI -->
-    {{-- @if(auth()->user()->role != 'kasir')
+        <!-- TRANSAKSI -->
+        {{-- @if (auth()->user()->role != 'kasir')
     <li class="menu-header small text-uppercase">
         <span class="menu-header-text">Transaksi</span>
     </li> --}}
 
-    <!-- Pembelian -->
-    {{-- <li class="menu-item {{ request()->is('pembelian*') ? 'active' : '' }}">
+        <!-- Pembelian -->
+        {{-- <li class="menu-item {{ request()->is('pembelian*') ? 'active' : '' }}">
         <a href="{{ route('pembelian.index') }}" class="menu-link">
             <i class="menu-icon tf-icons bx bx-cart"></i>
             <div>Pembelian</div>
         </a>
     </li> --}}
 
-    <!-- Produksi -->
-    {{-- <li class="menu-item {{ Route::currentRouteName() == 'produksi.index' ? 'active' : '' }}">
+        <!-- Produksi -->
+        {{-- <li class="menu-item {{ Route::currentRouteName() == 'produksi.index' ? 'active' : '' }}">
         <a href="{{ route('produksi.index') }}" class="menu-link">
             <i class="menu-icon tf-icons bx bx-package"></i>
             <div>Produksi</div>
@@ -51,16 +50,16 @@
     </li>
     @endif --}}
 
-    <!-- Penjualan (Tampilkan untuk semua) -->
-    {{-- <li class="menu-item {{ request()->is('penjualan*') ? 'active' : '' }}">
+        <!-- Penjualan (Tampilkan untuk semua) -->
+        {{-- <li class="menu-item {{ request()->is('penjualan*') ? 'active' : '' }}">
         <a href="{{ route('penjualan.index') }}" class="menu-link">
             <i class="menu-icon tf-icons bx bx-store"></i>
             <div>Penjualan</div>
         </a>
     </li> --}}
 
-    <!-- Gaji Karyawan -->
-    {{-- @if(auth()->user()->role != 'kasir')
+        <!-- Gaji Karyawan -->
+        {{-- @if (auth()->user()->role != 'kasir')
     <li class="menu-item {{ request()->is('gaji*') ? 'active' : '' }}">
         <a href="{{ route('gaji.index') }}" class="menu-link">
             <i class="menu-icon tf-icons bx bx-wallet"></i>
@@ -69,59 +68,59 @@
     </li>
     @endif --}}
 
-    <!-- MASTER DATA -->
-    @if(auth()->user()->role != 'kasir')
-    <li class="menu-header small text-uppercase">
-        <span class="menu-header-text">Master Data</span>
-    </li>
+        <!-- MASTER DATA -->
+        @if (auth()->user()->role != 'kasir')
+            <li class="menu-header small text-uppercase">
+                <span class="menu-header-text">Master Data</span>
+            </li>
 
-    <!-- Bahan Baku -->
-    {{-- <li class="menu-item {{ request()->is('bahan-baku*') ? 'active' : '' }}">
+            <!-- Bahan Baku -->
+            {{-- <li class="menu-item {{ request()->is('bahan-baku*') ? 'active' : '' }}">
         <a href="{{ route('bahan-baku.index') }}" class="menu-link">
             <i class="menu-icon tf-icons bx bx-box"></i>
             <div>Bahan Baku</div>
         </a>
     </li> --}}
 
-    <!-- Produk -->
-    <li class="menu-item {{ Route::currentRouteName() == 'produk.index' ? 'active' : '' }}">
-        <a href="{{ route('produk.index') }}" class="menu-link">
-            <i class="menu-icon tf-icons bx bx-package"></i>
-            <div>Produk</div>
-        </a>
-    </li>
+            <!-- Produk -->
+            <li class="menu-item {{ Route::currentRouteName() == 'produk.index' ? 'active' : '' }}">
+                <a href="{{ route('produk.index') }}" class="menu-link">
+                    <i class="menu-icon tf-icons bx bx-package"></i>
+                    <div>Produk</div>
+                </a>
+            </li>
 
-    <!-- Karyawan -->
-    <li class="menu-item {{ request()->is('karyawan*') ? 'active' : '' }}">
-        <a href="{{ route('karyawan.index') }}" class="menu-link">
-            <i class="menu-icon tf-icons bx bx-group"></i>
-            <div>Karyawan</div>
-        </a>
-    </li>
+            <!-- Karyawan -->
+            <li class="menu-item {{ request()->is('karyawan*') ? 'active' : '' }}">
+                <a href="{{ route('karyawan.index') }}" class="menu-link">
+                    <i class="menu-icon tf-icons bx bx-group"></i>
+                    <div>Karyawan</div>
+                </a>
+            </li>
 
-    <!-- Presensi -->
-    {{-- <li class="menu-item {{ request()->is('presensi*') ? 'active' : '' }}">
+            <!-- Presensi -->
+            {{-- <li class="menu-item {{ request()->is('presensi*') ? 'active' : '' }}">
         <a href="{{ route('presensi.index') }}" class="menu-link">
             <i class="menu-icon tf-icons bx bx-calendar-check"></i>
             <div>Presensi Karyawan</div>
         </a>
     </li>
-    @endif --}}
+   --}}
+        @endif
+        <!-- Owner -->
+        @if (auth()->user()->role == 'owner')
+            <li class="menu-header small text-uppercase">
+                <span class="menu-header-text">Owner</span>
+            </li>
 
-    <!-- Owner -->
-    @if(auth()->user()->role == 'owner')
-    <li class="menu-header small text-uppercase">
-        <span class="menu-header-text">Owner</span>
-    </li>
+            <li class="menu-item {{ request()->is('users*') ? 'active' : '' }}">
+                <a href="{{ route('users.index') }}" class="menu-link">
+                    <i class="menu-icon tf-icons bx bx-user"></i>
+                    <div>Manajemen User</div>
+                </a>
+            </li>
+        @endif
 
-    <li class="menu-item {{ request()->is('users*') ? 'active' : '' }}">
-        <a href="{{ route('users.index') }}" class="menu-link">
-            <i class="menu-icon tf-icons bx bx-user"></i>
-            <div>Manajemen User</div>
-        </a>
-    </li>
-    @endif
-
-</ul>
+    </ul>
 
 </aside>
